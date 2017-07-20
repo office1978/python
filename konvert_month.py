007b963015df02d423dbd5031a69034d505d9195
nosarev.a.n <office1978@list.ru>
2017-07-20 18:30:14 +0300

    думаю пока, как выкидывать ошибку о не правильном вводе

--- konvert_month.py
+++ konvert_month.py
@@ -1,18 +1,20 @@
 # -*- encoding: utf-8 -*-
-month_ru = {"январь":1, "февраль":2, "март":3, "апрель":4, "май":5,
-         "июнь":6, "июль":7, "август":8, "сентябрь":9,
-         "октябрь":10, "ноябрь":11, "декабрь":12}
-month_en = {"january":1, "february":2, "march":3, "april":4, "may":5,
-         "june":6, "july":7, "august":8, "september":9,
-         "october":10, "november":11, "december":12}
-d=(input("Введите название месяца: ")).lower()
-if month_ru.get(d) or month_en.get(d):
-    if 'а' <= d[0] <= 'я':
-        print(month_ru.get(d))
-    else:
-        print(month_en.get(d))
-else:
-    print ("Такого месяца нет.")
+year = {1:["январь","january"], 2:["февраль","february"], 3:["март", "march"], 4:["апрель", "april"], 5:["май", "may"],
+         6:["июнь","june"], 7:["июль", "july"], 8:["август", "august"], 9:["сентябрь", "september"],
+         10:["октябрь", "october"], 11:["ноябрь", "november"], 12:["декабрь", "december"]}
+
+d=input("Введите название месяца: ").lower()
+
+for k,v in year.items():
+    if d in v:
+        print (k)
+        
+
+
+
+
+
+    
 
 
     
